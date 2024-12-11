9a77b48e4d4efa543075031e2805d4c3bde2f601
frostycakes <lamquoc.24@gmail.com>
2024-12-11 17:44:54 -0500

    shadcn install

--- src/pages/UnderConstruction.tsx
+++ src/pages/UnderConstruction.tsx
@@ -6,6 +6,7 @@ import "./UnderConstruction.scss";
 import ArrowLeft from "../assets/arrow-left-bw.png";
 import CharlieChaplin from "../assets/charlie-chaplin.gif";
 
+
 const UnderConstruction = () => {
   const navigate = useNavigate();
   const closeHandler = () => {
@@ -22,13 +23,39 @@ const UnderConstruction = () => {
       </div>
       <img src={CharlieChaplin} alt="Stock photo of Charlie Chaplin" />
       <div className="under-construction-footer">
-        <button onClick={closeHandler}>
+        {/* <button onClick={closeHandler}>
           <img src={ArrowLeft} alt="Left arrow" />
           <p>Back to Home</p>
-        </button>
+        </button> */}
+          <RoundedSlideButton />
       </div>
     </div>
   );
 };
 
 export default UnderConstruction;
+
+const RoundedSlideButton = () => {
+  return (
+    <button
+      className={`
+        relative z-0 flex items-center gap-2 overflow-hidden rounded-lg border-[1px] 
+        border-violet-300 px-4 py-2 font-semibold
+        uppercase text-violet-300 transition-all duration-500
+        
+        before:absolute before:inset-0
+        before:-z-10 before:translate-x-[150%]
+        before:translate-y-[150%] before:scale-[2.5]
+        before:rounded-[100%] before:bg-violet-300
+        before:transition-transform before:duration-1000
+        before:content-[""]
+
+        hover:scale-105 hover:text-neutral-900
+        hover:before:translate-x-[0%]
+        hover:before:translate-y-[0%]
+        active:scale-95`}
+    >
+      <span>Sign up free</span>
+    </button>
+  );
+};
