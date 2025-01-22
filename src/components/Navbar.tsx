b34cd8c5425c59d62f7bcbbd550f2d1ef779ec65
frostycakes <lamquoc.24@gmail.com>
2025-01-22 17:12:59 -0500

    clerk nav component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,13 @@
 import { Link } from "react-router-dom";
 //Styles
 import "./Navbar.scss";
+//Auth
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 const Navbar = () => {
   return (
@@ -11,7 +18,13 @@ const Navbar = () => {
         <p>Daily Challenger</p>
       </Link>
       <div className="navbar-action-btns">
-        <Link to="/under-construction">Sign In</Link>
+        {/* <Link to="/under-construction">Sign In</Link> */}
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </nav>
   );
