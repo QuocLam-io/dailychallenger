1021af5c94e8f7340a2d9c5b97a2b31fc254da9e
frostycakes <lamquoc.24@gmail.com>
2025-01-28 12:47:03 -0500

    clerk imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.tsx";
 import LandingPage from "./pages/LandingPage.tsx";
 import UnderConstruction from "./pages/UnderConstruction.tsx";
+//Auth
+import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/clerk-react";
 
 function App() {
-
   // console.log("env test")
   // TODO: is this where I do the conditional?
   return (
