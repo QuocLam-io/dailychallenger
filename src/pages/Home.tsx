a4068abb84e72e4e3d7633706f1ee0c547585756
frostycakes <lamquoc.24@gmail.com>
2025-02-20 16:21:21 -0500

    fetch user info

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+//Styles
 import "./Home.scss";
+//Auth
+import { useUser } from "@clerk/clerk-react";
+//Backend
+import { supabase } from "../supabase-client.js";
 
 const Home = () => {
+  const { user } = useUser();
+  const name = user.primaryEmailAddress.emailAddress;
+  console.log(name, "name");
+
   return <main>Home</main>;
 };
 
