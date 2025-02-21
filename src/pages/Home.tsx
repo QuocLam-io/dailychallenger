7ac9a344ffa945fb0ef3e586eea00b66d9cfdb3a
frostycakes <lamquoc.24@gmail.com>
2025-02-21 14:30:19 -0500

    home check for user in user db

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,8 +8,35 @@ import { supabase } from "../supabase-client.js";
 
 const Home = () => {
   const { user } = useUser();
-  const name = user.primaryEmailAddress.emailAddress;
-  console.log(name, "name");
+
+  useEffect(() => {
+    const checkUser = async () => {
+      if (!user) return;
+
+      const email = user.primaryEmailAddress.emailAddress;
+
+      if (!email) return;
+
+      try {
+        const { data, error } = await supabase
+          .from("users")
+          .select("*")
+          .eq("email", email)
+          .single();
+
+        console.log(data, "data");
+
+        if (error && error.code !== "PGRST116") {
+          console.error("Error fetching user:", error);
+          return;
+        }
+
+
+      } catch (error) {}
+    };
+
+    checkUser();
+  }, []);
 
   return <main>Home</main>;
 };
