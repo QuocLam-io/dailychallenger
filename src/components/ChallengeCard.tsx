22cc32b735aebe6464379fc1d2f37388793c1295
frostycakes <lamquoc.24@gmail.com>
2025-05-16 18:10:56 -0400

    card deadline display fn

--- src/components/ChallengeCard.tsx
+++ src/components/ChallengeCard.tsx
@@ -4,6 +4,9 @@ import greenCheckmark from "@/assets/checkmark-green-circle.svg";
 import greyCheckmark from "@/assets/checkmark-grey-circle.svg";
 import greyEllipsis from "@/assets/vertical-ellipsis-grey.png";
 
+//Utils
+import { getDeadlineDisplay } from "@/utils/deadlineDisplay";
+
 //Types
 import { Challenge } from "@/stores/challengesStore";
 
@@ -14,18 +17,25 @@ type Props = {
 const ChallengeCard = ({ challenge }: Props) => {
   //TODO: time left utils
   //TODO: dropdown menu
-  //TODO: green checkmark logic
+
+  const completeChallengeHandler = (e: React.MouseEvent, id: string) => {
+    e.preventDefault();
+    console.log(id);
+  };
+
   return (
     <div className="challenge-card_wrapper">
       <span>{challenge.emoji}</span>
       <div className="titles">
         <h4>{challenge.title}</h4>
-        <p>{challenge.deadline}</p>
+        <p>{getDeadlineDisplay(challenge.deadline)} left</p>
       </div>
       <div className="card-status">
         <div className="card-status-done">
-          <img src={greenCheckmark} />
-          <p>Done</p>
+          <img src={challenge.is_completed ? greenCheckmark : greyCheckmark} />
+          <button onClick={(e) => completeChallengeHandler(e, challenge.id)}>
+            Done
+          </button>
         </div>
         <button>
           <img src={greyEllipsis} />
