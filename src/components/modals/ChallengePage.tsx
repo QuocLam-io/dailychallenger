6961f63b8ed237c224f6dc2303bc6cbd05394284
frostycakes <lamquoc.24@gmail.com>
2025-06-21 19:31:47 -0400

    challenge page some html

--- src/components/modals/ChallengePage.tsx
+++ src/components/modals/ChallengePage.tsx
@@ -2,13 +2,29 @@ import React from "react";
 import "./ChallengePage.scss";
 import Overlay from "../Overlay";
 import { useModalsStore } from "@/stores/modalsStore";
+import OldTimeyLamp from "@/assets/old-timey-lamp.png";
 
 const ChallengePage = () => {
   const challenge = useModalsStore((s) => s.challengePageChallenge);
-  console.log(challenge);
+
+  if (!challenge) return null;
+
   return (
     <Overlay>
-      <div className="challenge-page">Blub</div>
+      <div className="challenge-page">
+        <div className="challenge-page-hero">
+          <img src={OldTimeyLamp} alt="old timey lamp" width={80} height={80} />
+          <div className="challenge-page-hero-text">
+            <h2>I challenge myself to...</h2>
+            <p>{challenge.title}</p>
+          </div>
+        </div>
+        <div className="challenge-page-timer">
+          <p>Challenge ends in:</p>
+          <p>{/* TODO: Add timer logic here */}</p>
+        </div>
+        {/* TODO: Add footer and action buttons similar to FilledLanding */}
+      </div>
     </Overlay>
   );
 };
